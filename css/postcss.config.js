b7a699b5934b98d59416a49a47af5830479c0f66
givensuman <givensuman@duck.com>
2023-09-06 15:11:41 -0400

    postcss-each not working

--- css/postcss.config.js
+++ css/postcss.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   plugins: [
     // /build
+    // require("postcss-minify"),
+    // require("postcss-discard-comments"),
     require("postcss-apply"),
-    require("postcss-minify"),
-    require("postcss-discard-comments"),
-    require('postcss-each'),
+    require("postcss-each"),
     require("postcss-nested"),
+
     // /.storybook
     require("tailwindcss"),
     require("autoprefixer"),
--- css/src/button/Button.stories.js
+++ css/src/button/Button.stories.js
@@ -6,14 +6,60 @@ export default {
   },
 };
 
-export const Primary = {
+export const Default = {
   args: {
     class: "btn"
   },
 };
 
-export const ExtraSmall = {
+export const Static = {
   args: {
-    class: "btn is-xs"
+    class: "btn is-static"
+  }
+}
+
+export const Size = {
+  render: () => {
+    return `
+    <div>
+      <button class="btn is-xs">Click Me</button>
+      <button class="btn is-sm">Click Me</button>
+      <button class="btn">Click Me</button>
+      <button class="btn is-lg">Click Me</button>
+    </div>
+    `
+  }
+}
+
+export const Variants = {
+  render: () => {
+    return `
+    <div class="flex space-x-2">
+      <button class="btn">Click Me</button>
+      <button class="btn is-ghost">Click Me</button>
+      <button class="btn is-outline">Click Me</button>
+      <button class="btn is-link">Click Me</button>
+    </div>
+    `
+  }
+}
+
+export const IconButton = {
+  render: () => {
+    return `
+    <button class="icon-btn">:)</button>
+    `
+  }
+}
+
+export const ButtonGroup = {
+  render: () => {
+    return `
+    <div class="btn-group">
+      <button class="btn">Hello</button>
+      <button class="btn is-outline">There</button>
+      <button class="btn is-red is-lg">General Kenobi</button>
+    </div>
+    `
   }
 }
